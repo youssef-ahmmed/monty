4ab18694db592e8b9d8f23bc26e8ee8b6fdd7fad
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-23 00:13:18 +0300

    Update ordering functions of header file

--- monty.h
+++ monty.h
@@ -82,30 +82,30 @@ FILE *open_file(char *file_name);
 ssize_t read_line(size_t *buffer_size);
 char **tokenize_string(const char *delim);
 
-/** execute_instructions.c **/
+/** execute_instructions.c functions **/
 void (*get_operation(char *opcode))(stack_t **stack, unsigned int line_number);
 int execute_operations(char *file_name);
 int check_mode_comment(char **tokenized_str);
 
-/** logical_operators.c functions **/
-void swap_opcode(stack_t **stack, unsigned int line_number);
-void rotl_opcode(stack_t **stack, unsigned int line_number);
-void rotr_opcode(stack_t **stack, unsigned int line_number);
+/** update_mode.c function **/
+void update_mode(char *opcode);
 
 /** essential_errors.c functions **/
 void usage_error(void);
 void open_error(char *file_name);
 void malloc_error(void);
+
+/** opcode_errors1.c functions **/
 void invalid_instruction_error(unsigned int line_number);
 void two_elements_error(unsigned int line_number);
 
-/** opcode_errors1.c functions **/
+/** opcode_errors2.c functions **/
 void empty_stack_error(unsigned int line_number, char *opcode);
 void division_by_zero(unsigned int line_number);
 void ascii_out_of_range(unsigned int line_number, char *opcode);
 void push_non_integer(unsigned int line_number, char *opcode);
 
-/** linked list.c functions **/
+/** linked-list.c functions **/
 void print_list(const stack_t *head);
 size_t get_list_length(const stack_t *head);
 stack_t *add_node_at_first(stack_t **head, int n);
@@ -127,6 +127,11 @@ void div_opcode(stack_t **stack, unsigned int line_number);
 void mul_opcode(stack_t **stack, unsigned int line_number);
 void mod_opcode(stack_t **stack, unsigned int line_number);
 
+/** logical_operators.c functions **/
+void swap_opcode(stack_t **stack, unsigned int line_number);
+void rotl_opcode(stack_t **stack, unsigned int line_number);
+void rotr_opcode(stack_t **stack, unsigned int line_number);
+
 /** ascii_operations.c functions **/
 void pchar_opcode(stack_t **stack, unsigned int line_number);
 void pstr_opcode(stack_t **stack, unsigned int line_number);
@@ -135,8 +140,5 @@ void pstr_opcode(stack_t **stack, unsigned int line_number);
 void free_linked_list(stack_t *head);
 void free_tokenized_string(char **tokenized_str);
 void free_all_and_exit(void);
-
-/** update_mode.c function **/
-void update_mode(char *opcode);
 
 #endif
